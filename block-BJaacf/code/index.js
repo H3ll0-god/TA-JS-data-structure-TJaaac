ebc3abf0318bd13f8385ca73030f6921f39fbfbe
= <=>
2025-02-02 01:22:36 -0500

    Divisible is a bit doubtfull:

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,43 +1,50 @@
 // 1. Create an array named numbers and store 5 number values in it
 let numbers = [1,2,3,4,5,];
 // 2. Calculate the sum of array items and print it to the console using console.log()
-function sum(){
-    let add = 0
-    for(let i=0;i<numbers.length;i++){
-    add += numbers[i];
-    }
-    return add;
-}
-console.log(sum());
-// 3. Calculate the average of array items and print it to the console using console.log()
-function average(){
-    let add = 0
-    for(let i=0;i<numbers.length;i++){
-    add += numbers[i];
-    averageOfarray = add / numbers.length
-    }
-    return averageOfarray;
-}
-console.log(average());
-// 4. Find the highest number in the array and print it to the console using console.log()
-function highest(){
+let sum = 0;
 
-   // let add = 0
-   // for(let i=0;i<numbers.length;i++){
-    //add += numbers[i];
-    //averageOfarray = add / numbers.length
-   // }
-   // return averageOfarray;
+for(let number of numbers) {
+    sum += numberl;
 }
-console.log(average());
+console.log(sum);
+// 3. Calculate the average of array items and print it to the console using console.log()
+console.log(sum / numbers.length);
+// 4. Find the highest number in the array and print it to the console using console.log()
+let highestNumber = numbers [0];
+
+for(let number of numbers){
+    if(number > highestNumber){
+        highestNumber = number;
+    }
+}
+console.log(highestNumber);
 // 5. Find the lowest number in the array and print it to the console using console.log()
 
 // 6. Find the even numbers in the array and print them to the console using console.log()
-
+for(let number of numbers){
+    if (number % 2 ==0){
+        console.log(number,"Even");
+    }
+}
 // 7. Find the odd numbers in the array and print them to the console using console.log()
-
+for(let number of numbers){
+    if (number % 2 !==0){
+        console.log(number,"Odd");
+    }
+}
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
-
+for(let number of numbers){
+    if (number % 5 == 0){
+        console.log(number);
+    }
+}
 // 9. Log all the element of the array one by one
-
+for ( let number of numbers){
+    console.log(number,"Each value");
+}
 // 10. Find all the number in the array that is divisible by 3
+for(let number of numbers){
+    if (number % 3 == 0){
+        console.log(number);
+    }
+} 
